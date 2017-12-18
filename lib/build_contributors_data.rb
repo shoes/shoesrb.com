b7fec7cfae93ac2cae3ebe85e6445c6a2916782f
nichol alexander <nichol.alexander@gmail.com>
2017-12-18 11:38:19 -0500

    working and standardized.
    get members of the organization
    get the repos in the org
    get the contributors for each of the repos

--- lib/build_contributors_data.rb
+++ lib/build_contributors_data.rb
@@ -1,60 +1,104 @@
-require 'colors'
 require 'httparty'
 
-require 'pry'
+class Github
+  include HTTParty
+  base_uri 'https://api.github.com/'
 
-# build members hash
-members = {}
-
-# # make call to contributors.
-# response = HTTParty.get('https://api.github.com/orgs/shoes/members').parsed_response
-# keys_to_keep = ["id", "login", "url", "avatar_url"]
-# response.each do |member|
-#   github_id = member["id"]
-#   members[github_id] = member.select { |key, _value| keys_to_keep.include? key }
-# end
-
-# keys_to_keep = ["html_url", "name", "company", "blog", "location", "bio"]
-# members.each do |id, member_data|
-#   response = HTTParty.get(member_data["url"]).parsed_response
-#   member_data.merge! response.select { |key, _value| keys_to_keep.include? key }
-# end
-
-# # write to yaml file
-# File.open('_data/members.yml', 'w') {|f| f.write members.to_yaml } 
-
-
-# # build contributors
-contributors = {}
-# repos = {}
-
-# response = HTTParty.get('https://api.github.com/orgs/shoes/repos').parsed_response
-
-# keys_to_keep = ['name', 'html_url', 'url', 'open_issues_count', 'stargazers_count']
-# response.each do |repo|
-#   repo_github_id = repo['id']
-#   repos[repo_github_id] = repo.select { |key, value| keys_to_keep.include? key}
-# end
-
-# File.open('_data/repos.yml', 'w') {|f| f.write repos.to_yaml } 
-
-repos = YAML.load_file('_data/repos.yml')
-
-begin
-  keys_to_keep = ["id", "login", "url", "avatar_url", "contributions"]
-  repos.each do |id, repo_data|
-    contributors_url = repo_data['url'] + '/contributors'
-    @response = HTTParty.get(contributors_url).parsed_response
-    @response.each do |contributor|
-      id = contributor["id"]
-      contributors[id] = contributor.select {|key, value| keys_to_keep.include? key}
-      response = HTTParty.get(contributor["url"]).parsed_response
-      personal_keys = ["html_url", "name", "company", "blog", "location", "bio"]
-      contributors[id].merge! response.select { |key, value| personal_keys.include? key}
-    end 
+  def initialize
+    @auth = {'User-Agent' => 'nicholalexander',
+      'Authorization' => 'Bearer xxxxxx'
+    }
   end
-rescue
-  puts @response
-ensure
-  File.open('_data/contributors.yml', 'w') {|f| f.write contributors.to_yaml }
+
+  def members
+    resource = '/orgs/shoes/members'
+    response = self.class.get(resource, headers: @auth).parsed_response
+  end
+
+  def user(user_name)
+    resource = "/users/#{user_name}"
+    response = self.class.get(resource, headers: @auth).parsed_response
+  end
+
+  def repos
+    resource = "/orgs/shoes/repos"
+    response = self.class.get(resource, headers: @auth).parsed_response
+  end
+
+  def contributors(repo)
+    resource = "/repos/shoes/#{repo}/contributors"
+    response = self.class.get(resource, headers: @auth).parsed_response
+  end
 end
+
+class Parser
+  REPO_FIELDS = ['name', 'html_url', 'url', 'open_issues_count', 'stargazers_count']
+  MEMBER_FIELDS = ['id', 'login', 'url', 'avatar_url']
+  USER_FIELDS = ["html_url", "name", "company", "blog", "location", "bio"]
+  CONTRIBUTOR_FIELDS = ["id", "login", "url", "avatar_url", "contributions"]
+  
+  def self.members(data)
+    member_data = []
+    data.each do |member|
+      member_data << member.select { |key, _value| MEMBER_FIELDS.include? key }
+    end
+    return member_data
+  end
+
+  def self.users(data)
+    user_data = []
+    data.each do |user|
+      user_data << user_select(user)
+    end
+    return user_data
+  end
+
+  def self.user(data)
+    user_select(data)
+  end
+
+  def self.repos(data)
+    repo_data = []
+    data.each do |repo|
+      repo_data << repo.select{ |key, _value| REPO_FIELDS.include? key }
+    end
+    return repo_data
+  end
+
+  def self.contributor(data)
+    data.select { |key, value| CONTRIBUTOR_FIELDS.include? key }
+  end
+
+  private
+  def self.user_select(user_hash)
+    user_hash.select{|key, _value| USER_FIELDS.include? key}
+  end
+end
+
+
+# get organization members
+github = Github.new
+members = Parser.members(github.members)
+members.each do |member_hash|
+  user = github.user(member_hash['login'])
+  member_hash.merge! Parser.user(user)
+end
+File.open('_data/members.yml', 'w') { |f| f.write members.to_yaml }
+
+#get repos
+repos = Parser.repos(github.repos)
+File.open('_data/repos.yml', 'w') {|f| f.write repos.to_yaml } 
+
+# get info for contributors to each repo
+repos.each do |repo|
+  contributors = github.contributors(repo['name'])
+  contributors.map! do |contributor_hash|
+    contribution_data = Parser.contributor(contributor_hash)
+    user = github.user(contributor_hash['login'])
+    contribution_data.merge! Parser.user(user)
+  end
+  repo["contributors"] = contributors
+end
+File.open('_data/repo_with_contributors.yml', 'w') {|f| f.write repos.to_yaml}
+
+
